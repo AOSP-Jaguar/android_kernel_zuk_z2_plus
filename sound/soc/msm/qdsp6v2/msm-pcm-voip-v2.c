676dff42a442673c3d7b31f506fcb6e9a3f841ed
optimumpr <telez_one@yahoo.com>
2017-08-08 17:08:17 -0400

    ASoC: msm: qdsp6v2: return error when copy from userspace fails

--- sound/soc/msm/qdsp6v2/msm-pcm-voip-v2.c
+++ sound/soc/msm/qdsp6v2/msm-pcm-voip-v2.c
@@ -814,7 +814,17 @@ static int msm_pcm_playback_copy(struct snd_pcm_substream *substream, int a,
 			if (prtd->mode == MODE_PCM) {
 				ret = copy_from_user(&buf_node->frame.voc_pkt,
 							buf, count);
+				if (ret) {
+					pr_err("%s: copy from user failed %d\n",
+					       __func__, ret);
+					return -EFAULT;
+				}
 				buf_node->frame.pktlen = count;
+				if (ret) {
+					pr_err("%s: copy from user failed %d\n",
+					       __func__, ret);
+					return -EFAULT;
+				}
 			} else {
 				ret = copy_from_user(&buf_node->frame,
 							buf, count);
@@ -822,11 +832,6 @@ static int msm_pcm_playback_copy(struct snd_pcm_substream *substream, int a,
 					buf_node->frame.pktlen = count -
 					(sizeof(buf_node->frame.frm_hdr) +
 					 sizeof(buf_node->frame.pktlen));
-			}
-			if (ret) {
-				pr_err("%s: copy from user failed %d\n",
-				       __func__, ret);
-				return -EFAULT;
 			}
 			spin_lock_irqsave(&prtd->dsp_lock, dsp_flags);
 			list_add_tail(&buf_node->list, &prtd->in_queue);
